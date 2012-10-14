6a443e48c9fdca221ad9cbabdc17d2e7ceccee6b
Steven Rock <stevenrock@gmail.com>
2012-10-13 23:40:19 -0400

    Task heiarchy initial

--- src/Assignment.java
+++ src/Assignment.java
@@ -34,7 +34,105 @@ public class Assignment {
 
 	}
 	
+	public abstract class ANote{
+
+		private String name = null;
+		private String note = null;
+		private Date lastModifiedDate = null;
+		private Date createDate = null;
+		private String guid = null;
+		private boolean dirty = true;
+		
+		@Override
+		public String toString() {
+			return "ANote [name=" + name + ", note=" + note
+					+ ", lastModifiedDate=" + lastModifiedDate
+					+ ", createDate=" + createDate + ", guid=" + guid
+					+ ", dirty=" + dirty + "]";
+		}
+		
+		public boolean isDirty() {
+			return dirty;
+		}
+		public void setDirty(boolean dirty) {
+			this.dirty = dirty;
+		}
+		public String getGuid() {
+			return guid;
+		}
+		public void setGuid(String guid) {
+			this.guid = guid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getNote() {
+			return note;
+		}
+		public void setNote(String note) {
+			this.note = note;
+		}
+		public Date getLastModifiedDate() {
+			return lastModifiedDate;
+		}
+		public void setLastModifiedDate(Date lastModifiedDate) {
+			this.lastModifiedDate = lastModifiedDate;
+		}
+		public Date getCreateDate() {
+			return createDate;
+		}
+		public void setCreateDate(Date createDate) {
+			this.createDate = createDate;
+		}
+		
+		
+	}
+	
+	public class Note extends ANote {
+		
+		public Note(String name, String note, String guid) {
+			super();
+			setName(name);
+			setNote(note);
+			setGuid(guid);
+			setLastModifiedDate(GregorianCalendar.getInstance().getTime());
+			setCreateDate(GregorianCalendar.getInstance().getTime());
+			setDirty(true);
+			
+		}
+
+		public Note() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+		
+	}
+	public abstract class ATask extends Note{
+		
+		private boolean completed = false;
+		private Date completedDate = null;
+		
+		public boolean isCompleted() {
+			return completed;
+		}
+
+		public void setCompleted(boolean completed) {
+			this.completed = completed;
+			this.completedDate = GregorianCalendar.getInstance().getTime();
+		}
+		
+	}
+	
+	public class Task extends ATask{
+		
+	}
 	/*
+	 * 
 	 * Abstract Note
 	 * Note FlashCardNote ClassNote, Abstract Task
 	 * Task, DueDateTask, CalendarTask, InterviewTask
@@ -42,7 +140,7 @@ public class Assignment {
 	 * MultiTask, FollowUpTask, Chore Task, NestedTask
 	 */
 
-	private class Task{
+	private class Task1{
 
 		private String name = null;
 		private String note = null;
@@ -54,7 +152,7 @@ public class Assignment {
 		private Date completedDate = null;
 		
 		
-		public Task(String name, String note, Date dueDate) {
+		public Task1(String name, String note, Date dueDate) {
 			this.name = name;
 			this.note = note;
 			this.dueDate = dueDate;
@@ -62,7 +160,7 @@ public class Assignment {
 			this.lastModifiedDate = createDate;
 		}
 
-		public Task() {
+		public Task1() {
 			this.createDate = GregorianCalendar.getInstance().getTime();
 			this.lastModifiedDate = createDate;
 		}
